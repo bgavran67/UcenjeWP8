b0136456c347f898716343f2de6769ad65395e7c
bgavran67 <barbaragavran67@gmail.com>
2025-05-09 18:26:30 +0200

    kreiranje tablice GRUPE_bgavran67

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -4,12 +4,20 @@
 
 --use edunovawp8;
 
---nazivi tablica u množini zbog C i EF
-create table smjerovi(
-sifra int,
-naziv varchar(50),
-cijena decimal(18,2),
-datumpokretanja datetime,
-aktivan bit
-);
+--nazivi tablica u množini zbog C# i EF
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2),
+--datumpokretanja datetime,
+--aktivan bit
+--);
+
+--create table grupe(
+--sifra int,
+--naziv varchar(20),
+--smjer int,
+--predavac varchar(50)
+--);
+
 
