ad96cc6c171b877681567bf7184feed0dd4f9dba
bgavran67 <barbaragavran67@gmail.com>
2025-05-09 19:51:37 +0200

    izrada tablice OSOBA_bgavran67

---
+++ udruga za zastitu zivotinja_bgavran67.sql
@@ -0,0 +1,12 @@
+﻿--create database udrugazazastituzivotinja;
+
+--use udrugazazastituzivotinja;
+
+--drop table osoba;
+
+--create table osobe(
+--sifra int,
+--ime varchar (50),
+--prezime varchar (50)
+--);
+
