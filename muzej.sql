2e4ef76eedcc0b047ef91719dce3f4a8a08c5af3
bgavran67 <barbaragavran67@gmail.com>
2025-05-10 19:52:57 +0200

    Kreiranje tablice izlozba, i dodavanje brojeva u zagradama, zavsetak tablice

--- muzej.sql
+++ muzej.sql
@@ -2,22 +2,31 @@
 
 --use muzej;
 
---drop table djelo; 
+--drop table djela; 
 
 --create table djela(
 --sifra int,
---naziv varchar,
---umjetnik varchar
+--naziv varchar (50),
+--umjetnik varchar (50)
 --);
 
 --create table sponzori(
 --sifra int,
---naziv varchar,
---lokacija decimal
+--naziv varchar (50),
+--lokacija decimal (11,2)
 --);
+
 
 --create table kustosi(
 --sifra int,
---ime varchar,
---prezime varchar
+--ime varchar (50),
+--prezime varchar (50)
 --);
+
+--create table izlozba(
+--sifra int,
+--naziv varchar (50),
+--lokacija decimal(10,2),
+--tema varchar(100)
+--);
+
