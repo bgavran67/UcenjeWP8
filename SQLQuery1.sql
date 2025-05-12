56d9080afc106b1a53ec204ddca18bc8e162127d
bgavran67 <barbaragavran67@gmail.com>
2025-05-12 22:13:51 +0200

    kreiranje tablice taksi sluzbe

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿--create database taksisluzba;
+
+--use taksisluzba;
+
+--create table taksitvrtke(
+--sifra int,
+--ime varchar (100),
+--lokacija varchar (150),
+--tema varchar(70),
+--kustos int
+--);
