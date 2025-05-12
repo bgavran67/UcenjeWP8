110ba56afc5f252fa3efc125c553514191cf68ff
bgavran67 <barbaragavran67@gmail.com>
2025-05-12 22:27:02 +0200

    kreirana tablica doktori

---
+++ doktorskaordinacija.sql
@@ -0,0 +1,11 @@
+﻿--create database doktorskaordinacija;
+
+--use doktorskaordinacija;
+
+
+--create table doktori(
+--sifra int,
+--ime varchar (50),
+--prezime varchar (50),
+--email varchar (100)
+--);
