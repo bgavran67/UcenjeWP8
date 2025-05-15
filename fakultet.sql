7d2fd74f5fe0766d468cd6b38c4ee0405add4baf
bgavran67 <barbaragavran67@gmail.com>
2025-05-15 23:27:30 +0200

    Kreirana tablica fakultet

--- fakultet.sql
+++ fakultet.sql
@@ -1,28 +1,35 @@
-﻿--create database fakultet;
+﻿use master;
+go
+drop database if exists fakultet;
+go
+create database fakultet;
+go
+use fakultet;
 
---use fakultet;
+create table kolegiji(
+sifra int not null primary key identity(1,1),
+predmet varchar (100) not null
+);
 
---create table studenti(
---sifra int,
---ime varchar (50),
---prezime varchar (50)
---);
+create table prijave(
+sifra int not null primary key identity(1,1),
+studenti int not null,
+rokovi int not null,
+nazivpredmeta varchar (100) not null,
+vrijemedatum datetime not null
+);
 
---create table kolegiji(
---sifra int,
---predmet varchar (100)
---);
+create table rokovi(
+sifra int not null primary key identity(1,1),
+vrijemedatum datetime not null,
+kolegiji int not null references kolegiji(sifra)
+);
 
---create table prijave(
---sifra int,
---studenti int,
---rokovi int,
---nazivpredmeta varchar (100),
---vrijemedatum datetime
---);
+create table studenti(
+sifra int not null primary key identity(1,1),
+ime varchar (50) not null,
+prezime varchar (50) not null,
+prijave int not null references prijave(sifra),
+rokovi int not null references rokovi(sifra)
+);
 
---create table rokovi(
---sifra int,
---vrijemedatum datetime,
---kolegiji int
---);
