0e9fced5f26c8cbaf760aa1b912d4aa7156ac782
bgavran67 <barbaragavran67@gmail.com>
2025-05-15 23:53:41 +0200

    dovrsiti (zbog 1:1 izmedu vozaca i vozila)

--- taksi sluzba.sql
+++ taksi sluzba.sql
@@ -1,40 +1,43 @@
-﻿--create database taksisluzba;
+﻿use master;
+go
+drop database if exists taksisluzba;
+go
+create database taksisluzba;
+go
+use taksisluzba;
 
---use taksisluzba;
-
---create table taksitvrtke(
---sifra int,
---ime varchar (100),
---lokacija varchar (150),
---tema varchar(70),
---kustos int
---);
+create table taksitvrtke(
+sifra int not null primary key identity(1,1),
+ime varchar (100) not null,
+lokacija varchar (150)
+);
  
---create table vozila(
---sifra int,
---model varchar (50),
---tipvozila varchar (50),
---vozac int,
---);
+ create table vozaci(
+ sifra int not null primary key identity (1,1),
+ ime varchar(50) not null,
+ prezime varchar(60) not null
+ );
 
---create table vozaci(
---sifra int,
---ime varchar (50),
---prezime varchar (50)
---);
+create table putnici(
+sifra int not null primary key identity(1,1),
+ime varchar (50) not null,
+prezime varchar (50) not null,
+brojtelefona varchar(15) not null,
+adresa varchar(100) not null
+);
 
---create table putnici(
---sifra int,
---ime varchar (50),
---prezime varchar (50),
---brojtelefona varchar(15),
---adresa varchar(100)
---);
+create table vozila(
+sifra int not null primary key identity(1,1),
+model varchar (50) not null,
+tipvozila varchar (50) not null, 
+vozac int not null,
+taksitvrtka int not null references taksitvrtke(sifra)
+);
 
---create table voznje(
---sifra int,
---pocetakvoznje datetime,
---zavrsetakvoznje datetime,
---vozac int,
---putnik int
---);
+create table voznje(
+sifra int not null primary key identity(1,1),
+pocetakvoznje datetime not null,
+zavrsetakvoznje datetime not null,
+vozac int not null references vozaci(sifra),
+putnik int not null references putnici(sifra)
+);
