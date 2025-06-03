2c5e57a423a78017813a67066ca4a4f902b9ef1b
bgavran67 <barbaragavran67@gmail.com>
2025-06-03 19:52:39 +0200

    Ulaz Izlaz

---
+++ BACKEND/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E01UlazIzlaz
+    {
+
+
+        public static void Izvedi()
+        {
+
+
+
+            Console.Write("Pozdrav svijetu");
+
+            Console.WriteLine("Isti red");
+
+            Console.WriteLine("Osijek je \n SUPER"); // \n JE NOVI RED (\ alt gr Q)
+
+            Console.WriteLine("\t\tOvo je uvuceno");
+
+            Console.Write("Unesi svoje ime: ");
+
+            //varijabla ime
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unijeli ste {0}", ime);
+
+            
+
+
+        }//kraj metode
+
+
+
+
+    }//kraj klase
+}//kraj imenskog prostora (name space)
