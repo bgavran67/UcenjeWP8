a61ca3e61fd7768d8be298f3f04a144575b41003
bgavran67 <barbaragavran67@gmail.com>
2025-06-06 19:50:46 +0200

    Vjezba

---
+++ BACKEND/Ucenje/E02Z1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    class E02Z1
+    {
+
+        public static void Izvedi() 
+    {
+            Console.WriteLine("E02Z1");
+            Console.WriteLine("Unesi iznos place (pr. 132, 65: ");
+
+            double placa = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(placa);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+
+
+
+    }
+}
