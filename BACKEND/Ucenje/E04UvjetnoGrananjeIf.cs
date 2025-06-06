bb6da099ecd9f86a8f41d4162cbb00b12d953cfc
bgavran67 <barbaragavran67@gmail.com>
2025-06-06 19:51:10 +0200

    Uvjetno Grananje

---
+++ BACKEND/Ucenje/E04UvjetnoGrananjeIf.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    class E04UvjetnoGrananjeIf
+    {
+        public static void Izvedi()
+        {
+
+            Console.WriteLine("E04UvjetnoGrananjeIf");
+
+            int broj = 7; //ovo je unio korisnik
+
+            //operator =, rezultat je bool tip podatka (true, false)
+
+            bool uvjet = broj == 7;
+
+
+
+
+            //osnovna IF sintaksa - radi sa bool tipom podatka
+            if (uvjet)
+            {
+                Console.WriteLine("Super");
+            }
+            
+            //puna sintaksa IF naredbe
+            if(broj>2)
+            {
+                Console.WriteLine("OK");
+            }
+
+            else if(broj ==1)
+            {
+                Console.WriteLine("Nije dobro");
+            }
+
+            else
+            {
+                Console.WriteLine("NIJE OCJENA");
+            }
+
+            //operatori  < >, >=, <=, != se koriste u if-u
+
+            //logicki operatori and, or, not
+
+            // AND:
+            // &
+            //inace se koristi sa 2 AND (&)
+
+            int i = 5, j = 10;
+
+            if (i > 10 && j > 2)
+            {
+                Console.WriteLine("Nece se ispisati");
+            }
+
+         //OR
+         // | (Aaltgr + W)
+         
+            if (i==4 || j==2)
+            {
+                Console.WriteLine("Jedno od dvoje");
+            }
+
+
+         //NOT 
+         // !
+
+            if (!uvjet)
+            {
+                Console.WriteLine("Kada gore ne ispise, ovdje ce");
+            }
+
+
+            string grad = "Osijek";
+
+            if(uvjet && i > 10)
+            {
+                if (grad == "Vukovar")
+                {
+                    Console.WriteLine("Novi prvoligas");
+                }
+            }
+
+            if (grad == "Osijek")
+            {
+                Console.WriteLine("Super");
+            }
+            else
+            {
+                Console.WriteLine("OK");
+            }
+
+
+            // INLINE IF
+
+            Console.WriteLine(grad == "Osijek" ? "Super" : "OK"); 
+
+            
+
+
+
+
+
+        }
+
+    }
+}
---
+++ BACKEND/Ucenje/E05UvjetnoGrananjeSwitch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    class E05UvjetnoGrananjeSwitch
+    {
+        public static void Izvedi()
+        {
+
+            Console.WriteLine("E05UvjetnoGrananjeSwitch");
+
+            // SWITCH radi s tipovima podataka: int, string, char,...
+
+            Console.Write("Unesi ocjenu brojem: ");
+            int broj = int.Parse(Console.ReadLine());
+
+            switch (broj)
+            {
+                case 1:
+                    Console.WriteLine("Nedovoljan");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Dovoljan");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Dobar");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Vrlo Dobar");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Izvrstan");
+                    break;
+
+                case 6:
+
+                case 7:
+                    Console.WriteLine("Pa i to bi mogle biti ocjene ali nisu");
+                    break;
+
+                default:
+                    Console.WriteLine("nije ocjena");
+                    break;
+            }
+
+
+        }
+    }
+}
